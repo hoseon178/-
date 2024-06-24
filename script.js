3f50c445d12b7fc28c09b0248eea5c35787b0cf6
hoseon178 <did22557@naver.com>
2024-06-24 23:19:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuButton = document.getElementById("menuButton");
     const menuContent = document.getElementById("menuContent");
 
-    menuButton.addEventListener("click", function() {
+    menuButton.addEventListener("mousedown", function() {
         // toggle 메뉴 열기/닫기
         if (menuContent.style.display === "block") {
             menuContent.style.display = "none";
