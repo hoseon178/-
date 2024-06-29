f31b19063cedbb52c5c4a3b4a9a46c25e196655f
hoseon178 <did22557@naver.com>
2024-06-29 23:07:29 +0900

    Update std.js

--- std.js
+++ std.js
@@ -15,7 +15,7 @@ const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result() {
-  let newStory = ["It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."];
+  let newStory = storyText;
   
   const xItem = randomValueFromArray(insertX);
   const yItem = randomValueFromArray(insertY);
